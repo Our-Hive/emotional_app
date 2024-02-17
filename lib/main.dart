288888c39fbf6de195972b6bf95d737b12ba45c0
SnakyDH <skdanielg5@gmail.com>
2024-02-17 17:24:00 -0500

    feat: add principal colors and add rounded to filled buttoms

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:diary_emotional_frontend/config/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      darkTheme: AppTheme.dark(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -39,23 +37,78 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have sdasdsadapushed the button this many times:',
+              ),
+              Placeholder(
+                fallbackHeight: 100,
+                fallbackWidth: 100,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  filled: true,
+                ),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              SizedBox(height: 10),
+              FilledButton(
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(
+                        const Size(double.infinity, 50)),
+                  ),
+                  onPressed: () {
+                    print('object');
+                  },
+                  child: Text('data')),
+              SizedBox(height: 10),
+              FilledButton(
+                  style: FilledButton.styleFrom(
+                    backgroundColor: colors.error,
+                    foregroundColor: colors.onError,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {
+                    print('object');
+                  },
+                  child: Text('data')),
+              SizedBox(height: 10),
+              FilledButton(
+                  style: FilledButton.styleFrom(
+                    backgroundColor: colors.tertiary,
+                    foregroundColor: colors.onTertiary,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {
+                    print('object');
+                  },
+                  child: Text('data')),
+              SizedBox(height: 10),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
