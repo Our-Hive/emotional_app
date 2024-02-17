b326b95aeb57f10b4819ff134ef09373d5809b1d
SnakyDH <skdanielg5@gmail.com>
2024-02-17 17:48:18 -0500

    refactor: remove HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:diary_emotional_frontend/config/app_theme.dart';
+import 'package:diary_emotional_frontend/features/account/auth/ui/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,109 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       darkTheme: AppTheme.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have sdasdsadapushed the button this many times:',
-              ),
-              Placeholder(
-                fallbackHeight: 100,
-                fallbackWidth: 100,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  filled: true,
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-              SizedBox(height: 10),
-              FilledButton(
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(
-                        const Size(double.infinity, 50)),
-                  ),
-                  onPressed: () {
-                    print('object');
-                  },
-                  child: Text('data')),
-              SizedBox(height: 10),
-              FilledButton(
-                  style: FilledButton.styleFrom(
-                    backgroundColor: colors.error,
-                    foregroundColor: colors.onError,
-                    minimumSize: const Size(double.infinity, 50),
-                  ),
-                  onPressed: () {
-                    print('object');
-                  },
-                  child: Text('data')),
-              SizedBox(height: 10),
-              FilledButton(
-                  style: FilledButton.styleFrom(
-                    backgroundColor: colors.tertiary,
-                    foregroundColor: colors.onTertiary,
-                    minimumSize: const Size(double.infinity, 50),
-                  ),
-                  onPressed: () {
-                    print('object');
-                  },
-                  child: Text('data')),
-              SizedBox(height: 10),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
