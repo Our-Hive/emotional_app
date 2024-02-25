2f05e4580c177147dae95e9a07fd1188093a6a86
SnakyDH <skdanielg5@gmail.com>
2024-02-25 14:53:25 -0500

    refactor: use Token entity

--- lib/features/account/auth/infra/repo/auth_repo_impl.dart
+++ lib/features/account/auth/infra/repo/auth_repo_impl.dart
@@ -1,15 +1,17 @@
 import 'package:diary_emotional_frontend/features/account/auth/domain/data_source/auth_data_source.dart';
 import 'package:diary_emotional_frontend/features/account/auth/domain/entities/login.dart';
+import 'package:diary_emotional_frontend/features/account/auth/domain/entities/token.dart';
 import 'package:diary_emotional_frontend/features/account/auth/domain/repository/auth_repo.dart';
 
 class AuthRepoImpl implements AuthRepo {
   final AuthDataSource _authDataSource;
-  AuthRepoImpl(
-    this._authDataSource,
-  );
+
+  AuthRepoImpl({
+    required AuthDataSource authDataSource,
+  }) : _authDataSource = authDataSource;
 
   @override
-  Future<void> login(LoginCredentials authCredentials) async {
-    await _authDataSource.login(authCredentials);
+  Future<Token> login(LoginCredentials authCredentials) async {
+    return await _authDataSource.login(authCredentials);
   }
 }
