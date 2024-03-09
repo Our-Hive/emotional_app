2e074ba66e75220f96103d5250abf3d075df772c
SnakyDH <skdanielg5@gmail.com>
2024-03-08 23:14:15 -0500

    feat: add sign up credentials entity

---
+++ lib/features/account/auth/domain/entities/sign_up_credentials.dart
@@ -0,0 +1,19 @@
+class SignUpCredentials {
+  final DateTime birthDate;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String password;
+  final String phoneNumber;
+  final String username;
+
+  SignUpCredentials({
+    required this.birthDate,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.password,
+    required this.phoneNumber,
+    required this.username,
+  });
+}
