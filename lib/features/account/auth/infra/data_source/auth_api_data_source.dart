a80fdb85732b96c6eccae1ec4af3d732a3749ca1
SnakyDH <skdanielg5@gmail.com>
2024-03-08 23:26:41 -0500

    feat: add sign up with api

--- lib/features/account/auth/infra/data_source/auth_api_data_source.dart
+++ lib/features/account/auth/infra/data_source/auth_api_data_source.dart
@@ -5,28 +5,27 @@ import 'package:emotional_app/features/account/auth/domain/entities/sign_up_cred
 import 'package:emotional_app/features/account/auth/domain/entities/token.dart';
 import 'package:emotional_app/features/account/auth/infra/exceptions/invalid_credentials.dart';
 import 'package:emotional_app/features/account/auth/infra/mapper/token_login_api_mapper.dart';
-import 'package:emotional_app/features/account/auth/infra/model/login_api_response.dart';
+import 'package:emotional_app/features/account/auth/infra/model/auth_api_response.dart';
 import 'package:emotional_app/shared/exceptions/http_exception.dart';
 import 'package:dio/dio.dart';
 
 class AuthApiDataSourceImpl implements AuthDataSource {
   final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: AppEnvironment.apiUrl,
+      baseUrl: '${AppEnvironment.apiUrl}/auth',
     ),
   );
   @override
   Future<Token> login(LoginCredentials authCredentials) async {
     try {
       final response = await _dio.post(
-        '/auth/login',
+        '/login',
         data: {
           'email': authCredentials.email,
           'password': authCredentials.password,
         },
       );
-      return TokenLoginApiMapper.fromApi(
-          LoginApiResponse.fromJson(response.data));
+      return TokenApiMapper.fromApi(AuthApiResponse.fromJson(response.data));
     } on DioException catch (e) {
       if (e.response?.statusCode == 401) throw InvalidCredentialsException();
       throw HttpException();
@@ -36,8 +35,30 @@ class AuthApiDataSourceImpl implements AuthDataSource {
   }
 
   @override
-  Future<Token> signUp(SignUpCredentials signUpCredentials) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<Token> signUp(SignUpCredentials signUpCredentials) async {
+    try {
+      final response = await _dio.post(
+        '/signup',
+        data: {
+          'username': signUpCredentials.username,
+          'email': signUpCredentials.email,
+          'password': signUpCredentials.password,
+          'birthDate': signUpCredentials.birthDate,
+          'mobileNumber': signUpCredentials.phoneNumber,
+          'firstName': signUpCredentials.firstName,
+          'lastName': signUpCredentials.lastName,
+        },
+      );
+      return TokenApiMapper.fromApi(
+        AuthApiResponse.fromJson(
+          response.data,
+        ),
+      );
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 409) throw InvalidCredentialsException();
+      throw HttpException();
+    } catch (e) {
+      throw HttpException();
+    }
   }
 }
