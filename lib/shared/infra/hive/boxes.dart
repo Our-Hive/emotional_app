d80a4ff251b0713433553b034da75d65aabb19cf
SnakyDH <skdanielg5@gmail.com>
2024-03-09 22:57:37 -0500

    refactor: rename infra to infrastructure

--- lib/shared/infra/hive/boxes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:emotional_app/shared/infra/hive/hive_setup.dart';
-import 'package:hive/hive.dart';
-import 'package:emotional_app/features/account/auth/infra/model/auth_hive_model.dart';
-
-class Boxes {
-  static Box<AuthHiveModel> authBox() =>
-      Hive.box<AuthHiveModel>(HiveSetUp.authBox);
-}
--- lib/shared/infrastructure/hive/hive_setup.dart
+++ lib/shared/infrastructure/hive/hive_setup.dart
@@ -1,5 +1,5 @@
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:emotional_app/features/account/auth/infra/model/auth_hive_model.dart';
+import 'package:emotional_app/features/account/auth/infrastructure/model/auth_hive_model.dart';
 
 class HiveSetUp {
   static String authBox = 'auth_box';
