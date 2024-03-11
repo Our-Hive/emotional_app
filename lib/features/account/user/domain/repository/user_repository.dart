0ab5349152384a68f51e3106b4c86d61727fb4f1
SnakyDH <skdanielg5@gmail.com>
2024-03-10 20:30:03 -0500

    feat: add domain abstract class

---
+++ lib/features/account/user/domain/repository/user_repository.dart
@@ -0,0 +1,8 @@
+import 'package:emotional_app/features/account/auth/domain/entities/token.dart';
+import 'package:emotional_app/features/account/user/domain/entities/user.dart';
+
+abstract class UserRepository {
+  Future<User> getUser(Token token);
+  Future<User> updateUser(User user);
+  Future<User> deleteUser(String password);
+}
