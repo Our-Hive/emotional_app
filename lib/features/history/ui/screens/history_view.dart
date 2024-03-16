90e8c195b5a02d8b52236de95d0cf95b4380f72f
SnakyDH <skdanielg5@gmail.com>
2024-03-16 13:07:33 -0500

    feat: add emotion card widget

--- lib/features/history/ui/screens/history_view.dart
+++ lib/features/history/ui/screens/history_view.dart
@@ -1,3 +1,4 @@
+import 'package:emotional_app/shared/ui/emotion_card.dart';
 import 'package:flutter/material.dart';
 
 class HistoryView extends StatelessWidget {
@@ -10,11 +11,25 @@ class HistoryView extends StatelessWidget {
         title: const Text('History'),
       ),
       body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('History'),
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              EmotionCard(
+                primaryEmotion: 'Sorpresa',
+                secondaryEmotion: 'Efusivo',
+                primaryColor: Color(0xFF9C21E8),
+                bgColor: Color(0xFF28083C),
+              ),
+              SizedBox(height: 10),
+              EmotionCard(
+                secondaryEmotion: 'Sad',
+                primaryColor: Color(0xFF214DE8),
+                bgColor: Color(0xFF00061D),
+              ),
+            ],
+          ),
         ),
       ),
     );
